28ac7f43ec70984ed9796bae05c4f97de22b5b2a
slaveareyou <45574608+slaveareyou@users.noreply.github.com>
2018-12-05 15:34:24 +0300

    Update libft.h

--- srcs/libft.h
+++ srcs/libft.h
@@ -19,10 +19,10 @@
 
 typedef struct		s_list
 {
-	void			*content;
-	size_t			content_size;
+	void		*content;
+	size_t		content_size;
 	struct s_list	*next;
-}					t_list;
+}			t_list;
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
