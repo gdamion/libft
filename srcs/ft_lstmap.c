14ddc837d93e102180493d744eef93c5b8068de5
slaveareyou <45574608+slaveareyou@users.noreply.github.com>
2018-12-06 13:03:57 +0300

    Add files via upload

--- srcs/ft_lstmap.c
+++ srcs/ft_lstmap.c
@@ -14,26 +14,20 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list *buf;
-	t_list *nbuf;
-	t_list *nlst;
-	
+	t_list *start;
+	t_list *add;
+
 	if (!lst || !f)
 		return (NULL);
-	buf = lst;
-	nlst = ft_lstnew(0, sizeof(lst));	
-	nbuf = nlist;
-	while (buf->next != NULL)
+	if (!(add = f(lst)))
+		return (NULL);
+	start = add;
+	while (lst->next != NULL)
 	{
-		if (f(buf))
-		{
-		nlst->next =
-		}
-		buf = buf->next;
-
+		lst = lst->next;
+		if (!(add->next = f(lst)))
+			return (NULL);
+		add = add->next;
 	}
-	nlst->next = NULL;
-	return (nbuf);
-	}
-	return (NULL);
+	return (start);
 }
